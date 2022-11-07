eed5040ecce9213c15fcdf62d163652e283627f4
Jagrati Choure <114644426+jgchoure19@users.noreply.github.com>
2022-11-07 19:35:04 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (8.65%) - LeetHub

---
+++ 1323-maximum-69-number/1323-maximum-69-number.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int maximum69Number (int num) {
+        
+        int rightcnt=-1;
+        int inital =0 ;
+        int temp = num ;
+        
+        while( temp>0){
+            
+            int number= temp%10;
+            if(number==6) rightcnt= inital ;
+            
+            inital++;
+            temp/=10;
+            
+        }
+        
+        if(rightcnt==-1)  return num;
+        
+        int ans =num+(3* pow(10 , rightcnt));
+        return ans;
+       
+    }
+};
