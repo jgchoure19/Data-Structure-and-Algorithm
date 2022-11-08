c145fa0aa5ecc671c1bb66fd6eac623307c452ed
Jagrati Choure <114644426+jgchoure19@users.noreply.github.com>
2022-11-08 15:15:20 +0530

    Time: 4 ms (24.91%), Space: 6.4 MB (90.33%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.cpp
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -3,6 +3,7 @@ public:
     vector<vector<int>> generate(int numRows) {
         
         vector<vector<int>> r(numRows);
+        
           for (int i = 0; i < numRows; i++) {
             r[i].resize(i + 1);
             r[i][0] = r[i][i] = 1;
