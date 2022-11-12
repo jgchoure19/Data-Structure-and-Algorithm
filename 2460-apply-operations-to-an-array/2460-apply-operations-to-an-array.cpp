65d22976c63c091171e36102a47496fe31ed65f9
Jagrati Choure <114644426+jgchoure19@users.noreply.github.com>
2022-11-12 13:43:15 +0530

    Time: 6 ms (70.91%), Space: 8.9 MB (18.21%) - LeetHub

--- 2460-apply-operations-to-an-array/2460-apply-operations-to-an-array.cpp
+++ 2460-apply-operations-to-an-array/2460-apply-operations-to-an-array.cpp
@@ -1,31 +1,21 @@
 class Solution {
 public:
-    vector<int> applyOperations(vector<int>& nums) {
+    void moveZeroes(vector<int>& nums) {
         
-      
-        int n = nums.size();
-        
-        for( int i=0 ; i< n-1; i++){
-            
-            if( nums[i]==nums[i+1]){
-                
-                nums[i]*=2;
-                nums[i+1]=0;
-            }
-        }
-        
-         int k=0 ;
-        vector<int> ans(n,0);
-        
-        for( int i=0 ;i<n ; i++){
-            
-            if(nums[i]!=0){
-                             
-                ans[k]=nums[i];
+        int n=nums.size();
+        int k=0;
+         for( int i =0 ; i<n; i++){
+             
+             if(nums[i]!=0){
+             
+                int temp = nums[i];
+                nums[i] = nums[k];
+                nums[k] = temp;
                 k++;
-            }
-            
-        }
-         return ans;
+             
+             }
+             
+         }
+        
     }
 };
