3c050b23c0f8c01051af98186e23cde77a45c6b3
Jagrati Choure <114644426+jgchoure19@users.noreply.github.com>
2022-12-04 18:30:39 +0530

    Time: 30 ms (25.70%), Space: 5.9 MB (38.51%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -2,16 +2,6 @@ class Solution {
 public:
     int fib(int n) {
         
-       /* int fb[40];
-        
-        fb[0] =0 ;
-        fb[1] =1;
-        
-        for( int i= 2 ; i<=n ; i++){
-            
-            fb[i] = fb[i-2]+fb[i-1];
-        }
-        return fb[n];*/
         
         if(n ==0 || n==1)  return n ;
         
