56e07a33a3174836315d82a14a2b1b66c09c8cb5
Jagrati Choure <114644426+jgchoure19@users.noreply.github.com>
2022-12-04 17:51:27 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (30.84%) - LeetHub

---
+++ 7-reverse-integer/7-reverse-integer.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int reverse(int x) {
+       
+        long rev =0 ;
+         int n =x;
+          if(x < 0) x = abs(x);
+        
+              
+        while(x>0){
+            
+            rev=rev*10+x%10;
+            x/=10;
+        }
+        
+        if(rev>=pow(2,31)) return 0;
+        
+        if(n<0)  return -rev;
+            return rev;
+     
+        
+    }
+};
